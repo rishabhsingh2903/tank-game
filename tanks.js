de652d379be1200cc81600c68125015e1751fef1
rishabhsingh2903 <rishabhvns2000@gmailcom>
2022-12-10 19:12:59 -0600

    edited the audio file for the game

--- tanks.js
+++ tanks.js
@@ -22,7 +22,7 @@ mess();
 //audio for shooting
 var firesound=new Audio("./public/shoot.mp3");
 //audio when game is started
-var audio=new Audio("./public/got.mp3");
+var audio=new Audio("./public/startgamefinal.mp3");
 //audio when space ship is moved
 var tankmove=new Audio("./public/spaceshipmove.mp3");
 //to check wether the game is running or not
